e2166b67c58ecc70d5bf66d9a5cdfaf2865ff71b
Lubov Sokolova <falke.liebe@gmail.com>
2022-06-07 20:26:42 +0800

    add changing in project

---
+++ Lesson 1/SupportLibrary/SupportCommand.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading;
+
+namespace SupportLibrary
+{
+    public class SupportCommand
+    {
+        public static void Print(string ms, int x, int y)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.WriteLine(ms);
+            y = Console.CursorTop;
+        }
+
+        public static void Pause(int ms)
+        {
+            Thread.Sleep(ms);
+        }
+    }
+}
